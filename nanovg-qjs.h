081c8bde25ec44280abd366a7cc511f0765ce1cf
Roman Senn <roman.l.senn@gmail.com>
2021-03-13 20:16:49 +0100

    ...

--- nanovg-qjs.h
+++ nanovg-qjs.h
@@ -3,13 +3,12 @@
 #include <quickjs.h>
 
 #ifdef JS_SHARED_LIBRARY
-#define js_init_module_nanovg  js_init_module
+#define js_init_module_nanovg js_init_module
 #endif
 
 struct NVGcontext;
-void js_nanovg_init_with_context(struct NVGcontext *vg);
+void js_nanovg_init_with_context(struct NVGcontext* vg);
 
-JSModuleDef *js_init_module_nanovg(JSContext *ctx, const char *module_name);
-
+JSModuleDef* js_init_module_nanovg(JSContext* ctx, const char* module_name);
 
 #endif /* NANOVG_QJS_H */
